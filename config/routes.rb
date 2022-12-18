f409fc8f0f8401d36437bbc44de83a1445b03b96
captproton <carl@wdwhub.net>
2022-12-17 17:01:38 -0800

    INIT

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :announcements, only: [:index], export: true
+  devise_for :users
+  root to: 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
