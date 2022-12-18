8b5b7a621135852edb6767dc464494c19edca8b6
captproton <carl@wdwhub.net>
2022-12-17 17:39:28 -0800

    ADD: vite reload,vite vue plugin, page to demo vue

---
+++ app/helpers/palooza_counter_helper.rb
@@ -0,0 +1,2 @@
+module PaloozaCounterHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   resources :announcements, only: [:index], export: true
   devise_for :users
   root to: 'home#index'
